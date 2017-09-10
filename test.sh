12366387dd15992f5967f53d597a5bce237bf6bc
jiangxiaoqiang <jiangtingqiang@gmail.com>
2017-09-10 14:11:27 +0800

    接口请求认证添加排序

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ readonly APP_ID="hlb1451j6d136334gh2"
 readonly APP_KEY="test"
 ECHOSTR=`head -n 1 /dev/urandom|sed 's/[^a-z0-9]//g'|md5sum|awk '{print $1}'`
 CURRENT_TIME=`date '+%Y-%m-%d %H:%M:%S'`
-CONST_STR="ysf"
+#CONST_STR="ysf"
 #
 # 生成TOKEN
 # hlb1451j6d136334gh2接口各项认证参数的排列顺序是：
@@ -15,13 +15,13 @@ CONST_STR="ysf"
 # 第二个参数用\$2接收，以此类推
 # \$@表示所有的参数，相当于Java中main函数中定义的参数数组
 #
-TOKEN=`echo -n ${APP_ID}${APP_KEY}${CURRENT_TIME}${CONST_STR}|md5sum|awk '{print $1}'`
+#TOKEN=`echo -n ${APP_ID}${APP_KEY}${CURRENT_TIME}${ECHOSTR}|md5sum|awk '{print $1}'`
 
-aSEQUENCE_TOKEN=`echo -n ${CURRENT_TIME}_${APP_ID}_${APP_KEY}_${CONST_STR}|tr '_' '\t'|sort|tr -d '\011'`
-SEQUENCE_TOKEN=`echo -n ${CURRENT_TIME}${APP_ID}${APP_KEY}${CONST_STR}|shasum -a 1|awk '{print $1}'`
-
+SORTED_TOKEN=`echo -n ${CURRENT_TIME}_${APP_ID}_${APP_KEY}_${ECHOSTR}|tr '_' '\t'|sort|tr -d '\011'`
+#SEQUENCE_TOKEN=`echo -n ${CURRENT_TIME}${APP_ID}${APP_KEY}${CONST_STR}|shasum -a 1|awk '{print $1}'`
+TOKEN=`echo -n ${SORTED_TOKEN}|md5sum|awk '{print $1}'`
 # 请求服务端
-COMMAND=`curl -H "APPID:$APP_ID" -H "TIMESTAMP:$CURRENT_TIME" -H "ECHOSTR:$CONST_STR" -H "TOKEN:$SEQUENCE_TOKEN" \
+COMMAND=`curl -H "APPID:$APP_ID" -H "TIMESTAMP:$CURRENT_TIME" -H "ECHOSTR:$CONST_STR" -H "TOKEN:SORTED_TOKEN" \
 	http://localhost:28080/api/blacklist/page?xdr= | jq '.'`
 echo -n 'b a'|tr ' ' '\t' |sort|tr -d '\040\011\012\015'
 
